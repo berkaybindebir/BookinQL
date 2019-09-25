df85d17c9b21a55122a68f863d2708792101cd0f
Berkay Bindebir <berkay.bindebir@smartface.io>
2019-09-25 15:38:39 +0300

    Connect to the mlab server

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
+import mongoose from "mongoose";
 import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
 
 require("dotenv").config();
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, MLAB_USER, MLAB_PASSWORD, MONGO_URI } = process.env;
 
-const app = express();
-app.disable("x-powered-by");
+(async () => {
+	try {
+		await mongoose.connect(
+			`mongodb://${MLAB_USER}:${MLAB_PASSWORD}@${MONGO_URI}`,
+			{ useNewUrlParser: true }
+		);
 
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	playground: NODE_ENV === "DEVELOPMENT"
-});
+		const app = express();
+		const server = new ApolloServer({
+			typeDefs,
+			resolvers,
+			playground: NODE_ENV === "DEVELOPMENT"
+		});
 
-server.applyMiddleware({ app });
+		server.applyMiddleware({ app });
 
-app.listen(PORT, () => {
-	console.info("Server is running on", PORT);
-});
+		app.listen(PORT, () => {
+			console.info("Server is running on", PORT);
+		});
+	} catch (e) {
+		console.error(e);
+	}
+})();
